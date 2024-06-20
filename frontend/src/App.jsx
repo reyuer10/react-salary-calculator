0c55b4a3176c832e49df0ad2e88402ce890552e6
reyuer <reyuer10@gmail.com>
2024-06-20 09:01:43 +0800

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl underline">
+      <p className="font-play">Hello world!</p>
+      <p className=" font-orbitron">1234567890</p>
+    </h1>
+  );
+}
---
+++ frontend/src/components/SalaryInputs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SalaryInputs() {
+  return (
+    <div>SalaryInputs</div>
+  )
+}
+
+export default SalaryInputs
